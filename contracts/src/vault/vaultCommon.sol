8aa6addc42020486323923fde91762953738ba55
ariyan-hashstack <ariyan_hashstack@hashstack.finance>
2026-08-26 17:31:00 +0530

    [contracts] common impl

---
+++ contracts/src/vault/vaultCommon.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import { AccessControlUpgradeable } from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import { ReentrancyGuardTransient } from "@openzeppelin/contracts/utils/ReentrancyGuardTransient.sol";
+import { UUPSUpgradeable } from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import { ZeroAddress } from "../errors/Errors.sol";
+
+abstract contract VaultCommon is 
+    PausableUpgradeable, 
+    AccessControlUpgradeable, 
+    ReentrancyGuardTransient, 
+    UUPSUpgradeable {
+    bytes32 public constant GOVERNOR_ROLE = keccak256("GOVERNOR_ROLE");
+    bytes32 public constant RELAYER_ROLE = keccak256("RELAYER_ROLE");
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function __VaultCommon_init(address admin) internal onlyInitializing {
+        if (admin == address(0)) revert ZeroAddress();
+        __AccessControl_init();
+        __Pausable_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(GOVERNOR_ROLE, admin);
+    }
+
+    function pause() external onlyRole(GOVERNOR_ROLE) {
+        _pause();
+    }
+
+    function unpause() external onlyRole(GOVERNOR_ROLE) {
+        _unpause();
+    }
+
+    function _authorizeUpgrade(address) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
+}
